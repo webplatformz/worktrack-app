5e0e14f73bfa0f0d292b49d975427980d55bc5d3
Pascal Kaufmann <ookomo.dev@gmail.com>
2015-06-30 14:05:44 +0200

    refactor client app infrastructure

---
+++ client/app/scripts/app.js
@@ -0,0 +1,4 @@
+'use strict'
+var React = window.React = require('react'),
+    Hello = require("./ui/helloworld");
+
---
+++ client/app/scripts/ui/helloworld.js
@@ -0,0 +1,8 @@
+'use strict'
+
+var React = require('react');
+
+React.render(
+  React.createElement('h1', null, 'Hello, world!'),
+  document.getElementById('example')
+);
