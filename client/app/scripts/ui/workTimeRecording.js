d7a9ed8094f595ff0c87e948081faac1b0fcba11
Pascal Kaufmann <ookomo.dev@gmail.com>
2015-07-03 09:47:06 +0200

    please ignore this commit

---
+++ client/app/scripts/ui/workTimeRecording.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var WorkTimeRecordBox = React.createClass({
+    render: function () {
+        return (
+            <div>
+                hi
+            </div>
+        );
+    }
+});
+
+React.render(<WorkTimeRecordBox />, document.getElementById('recording'));
